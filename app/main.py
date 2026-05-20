c2360286d4f61819a4bc8e4fe4522543d69b5dd9
VicKingo98 <victorosorio592@gmail.com>
2026-05-20 17:28:26 -0500

    add endpoint healthcheck

--- app/main.py
+++ app/main.py
@@ -40,6 +40,12 @@ def root():
         "message": "Movie Genre Classifier API running"
     }
 
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
+
 # =========================================
 # PREDICT ENDPOINT
 # =========================================
